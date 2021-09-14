818a1c837e8d284051b6e01064ced5c82d6ff71e
shawn.yang <shawn.yang@idtechproducts.com>
2021-09-14 09:34:21 +0000

    first console app

---
+++ firstApp/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace firstApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
